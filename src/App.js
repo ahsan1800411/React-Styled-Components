3a36f49726d8539e484a723017df40e3ac2eca08
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-12-11 19:40:28 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import styled, {
+  keyframes,
+  ThemeProvider,
+  createGlobalStyle,
+} from 'styled-components';
+
+const ButtonStyled = styled.button`
+  background-color: ${({ variant }) =>
+    variant === 'outlined' ? 'red' : 'green'};
+  color: #fff;
+`;
+const FancyButtonStyled = styled(ButtonStyled)`
+  border: 2px solid tomato;
+  border-radius: 20px;
+
+  &:hover {
+    cursor: pointer;
+    background-color: orange;
+  }
+`;
+
+const NewButtonStyled = styled(ButtonStyled).attrs((props) => ({
+  type: 'submit',
+}))`
+  color: 'blue';
+  &:hover {
+    background-color: blue;
+    cursor: pointer;
+  }
+`;
+
+const rotate = keyframes`
+from{
+  transform:rotate(0deg)
+}to {
+  transform:rotate(360deg)
+}
+`;
+
+const AnimatedLogo = styled.img`
+  height: 30vmin;
+  pointer-events: none;
+  animation: ${rotate} infinite 10s linear;
+`;
+
+const theme = {
+  dark: {
+    primary: '#000',
+    text: '#fff',
+  },
+  light: {
+    primary: '#fff',
+    text: '#000',
+  },
+};
+
+const We = styled.button`
+  color: ${({ theme }) => theme.dark.primary};
+`;
+
+const GlobalStyle = createGlobalStyle`
+button{
+  font-family:"Roboto"
+}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className='App'>
+        <ButtonStyled variant='outlin'>Home</ButtonStyled>
+        <hr />
+        <FancyButtonStyled as='a'>Hii</FancyButtonStyled>
+        <NewButtonStyled>Munna</NewButtonStyled>
+        <AnimatedLogo src={logo} />
+        <We>We are</We>
+      </div>
+    </ThemeProvider>
   );
 }
 
